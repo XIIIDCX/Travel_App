41379ec68b59f11f04507856b61d1924474ea0ad
Damian <xiiidcx@gmail.com>
2023-11-06 23:12:24 +0100

    Hero section setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,7 +33,8 @@ const Navbar = () => {
           src='menu.svg'
           alt='menu'
           width={32}
-          
+          height={32}
+          className="inline-block cursor-pointer lg:hidden"
         />
       
     </nav>
